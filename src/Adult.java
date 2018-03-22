7c8102b5f05f431080389fd40815958e7fe597a6
rmit-s3679718-natalie-sy <s3679718@student.rmit.edu.au>
2018-03-22 11:36:47 +1100

    AddComments

--- src/Adult.java
+++ src/Adult.java
@@ -2,6 +2,7 @@
 public class Adult extends User {
 
 	public Adult (String userName, String image, String userStatus, int userAge, String child) {
+		//subclass
 		super(userName, image, userStatus, userAge);
 		child = child;
 	}
